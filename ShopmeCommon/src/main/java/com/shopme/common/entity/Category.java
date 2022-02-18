cec09ad537363b8282cae9e8aa3826bad428c340
Alan Zurita <alan.zurita@dinamica.local>
2022-02-18 19:57:40 -0300

    paginacion de categorias

--- ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Category.java
@@ -12,6 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity
 @Table(name = "categories")
@@ -45,6 +46,41 @@ public class Category {
 	public Category(Integer id) {
 		this.id = id;
 	}
+	
+	public static Category copyIdAndName (Category category) {
+		Category copyCategory = new Category();
+		copyCategory.setId(category.getId());
+		copyCategory.setName(category.getName());
+		
+		return copyCategory;
+	}
+	
+	public static Category copyIdAndName (Integer id, String name) {
+		Category copyCategory = new Category();
+		copyCategory.setId(id);
+		copyCategory.setName(name);
+		
+		return copyCategory;
+	}
+	
+	public static Category copyFull (Category category) {
+		Category copyCategory = new Category();
+		copyCategory.setId(category.getId());
+		copyCategory.setName(category.getName());
+		copyCategory.setImage(category.getImage());
+		copyCategory.setAlias(category.getAlias());
+		copyCategory.setEnabled(category.isEnabled());
+		copyCategory.setHasChildren(category.getChildren().size() > 0);
+		
+		return copyCategory;
+	}
+	
+	public static Category copyFull (Category category, String name) {
+		Category copyCategory = Category.copyFull(category);
+		copyCategory.setName(name);
+		
+		return copyCategory;
+	}
 
 	public Category(String name) {
 		this.name = name;
@@ -55,6 +91,12 @@ public class Category {
 	public Category(String name, Category parent) {
 		this(name);
 		this.parent = parent;
+	}
+	
+	public Category(Integer id, String name, String alias) {
+		this.id = id;
+		this.name = name;
+		this.alias = alias;
 	}
 
 	public Integer getId() {
@@ -113,5 +155,21 @@ public class Category {
 		this.children = children;
 	}
 	
+	@Transient
+	public String getImagePath() {
+		if (this.id == null) return "/images/image-thumbnail.png";
+		
+		return "/category-images/" + this.id + "/" + this.image;
+	}
 	
+	public boolean isHasChildren() {
+		return hasChildren;
+	}
+	
+	public void setHasChildren(boolean hasChildren) {
+		this.hasChildren = hasChildren;
+	}
+	
+	@Transient
+	private boolean hasChildren;
 }
